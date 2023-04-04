563b0c141fdc4125ab4a77c4699d72f09fe69b52
DDuQ <estebanduque2@gmail.com>
2023-04-03 19:09:44 -0500

    feat(init): Initial commit.

---
+++ Gh.Actions.Poc/Gh.Actions.Poc/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Pet.Project.Auth0.Authorization;
+using System.Security.Claims;
+
+namespace Pet.Project.Auth0.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAuth0Authentication(this IServiceCollection services, string authority, string audience)
+        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.Authority = authority;
+                    options.Audience = audience;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        NameClaimType = ClaimTypes.NameIdentifier
+                    };
+                });
+
+            return services;
+        }
+
+        public static IServiceCollection AddAuth0Authorization(this IServiceCollection services, string authority)
+        {
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("read:messages", policy => policy.Requirements.Add(new HasScopeRequirement("read:messages", authority)));
+            });
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
+
+            return services;
+        }
+    }
+}
